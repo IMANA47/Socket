fd5d171fec8f38d8c6a7d075e8959ed9d822218c
Francois Nsengimana <francknsengimana@gmail.com>
2026-06-12 10:56:28 +0100

    Refactor client connection handling with error management

--- sockets_client.py
+++ sockets_client.py
@@ -3,7 +3,11 @@ HOST_IP = "127.0.0.1"
 HOST_PORT = 32000
 
 client = socket.socket()
-print(f"Connexion au serveur {HOST_IP} sur le port {HOST_PORT}...")
-client.connect((HOST_IP, HOST_PORT))
-print("Connexion établie avec le serveur.")
-client.close()
+try:
+    client.connect((HOST_IP, HOST_PORT))
+    print(f"Connexion au serveur {HOST_IP} sur le port {HOST_PORT}...")
+    print("Connexion établie avec le serveur.")
+except ConnectionRefusedError:
+    print("Connexion au serveur impossible.")
+finally:
+    client.close()
